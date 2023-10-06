216d966d3a3116e31cc5f7ee25965b04788d23b2
kai zhu <kaizhu256@gmail.com>
2023-10-06 00:08:30 -0500

    tweak

--- test.py
+++ test.py
@@ -10,7 +10,9 @@ from scipy import sparse
     # !! from lightgbm.basic import _LIB as LIB
 # !! except ModuleNotFoundError:
     # !! print("Could not import lightgbm Python package, looking for lib_lightgbm at the repo root")
-if system() in ('Windows', 'Microsoft'):
+if system() in ('Darwin'):
+    lib_file = "./lib_lightgbm.dll"
+else if system() in ('Windows', 'Microsoft'):
     lib_file = "./lib_lightgbm.dll"
 else:
     lib_file = "./lib_lightgbm.so"
